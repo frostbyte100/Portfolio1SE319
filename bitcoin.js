b17ef8cef8c2c34c67aaada614de381bd0ed5736
Leelabari Fulbel <Leelabari Fulbel>
2017-02-11 13:11:54 -0600

    working on the showing of bitcoin blocks

--- bitcoin.js
+++ bitcoin.js
@@ -4,16 +4,54 @@ var transactions = new Array();
 var exchangeRate;
 var lastBlockNum;
 
+function Block(blockNum, numTx, date) {
+    this.blockNum = blockNum;
+    this.numTx = numTx;
+    this.date = date;
+}
+
+var Blocks = new Array();
+
 window.onload = new function(){
   //Gets Lasts Block's NUmber
   $.getJSON("http://btc.blockr.io/api/v1/block/raw/last", function(data){
     lastBlockNum = data["data"]["height"];
-    console.log(lastBlockNum);
+    //console.log(data);
   });
   $.getJSON("http://btc.blockr.io/api/v1/exchangerate/current", function( data ) {
     exchangeRate = data["data"][0]["rates"]["BTC"];
   });
 }
+
+function getNFirstBlocks(n){
+  var i=0;
+  for(i=0;i<n;i++){
+    getABlock(i);
+  }
+}
+
+
+function getNLastBlocks(n){
+  var i=n;
+  for(i=n;i>0;i--){
+    getABlock(i);
+  }
+}
+
+
+function getABlock(n){
+  $.getJSON("http://btc.blockr.io/api/v1/block/raw/"+n, function(data){
+    //lastBlockNum = data["data"]["height"];
+    console.log(data["data"]["tx"].length);
+    console.log(new Date( parseInt(data["data"]["time"])*1000 ));
+    console.log(new Date(data["data"]["time"]).getYear() );
+    var b = new Block(n,data["data"]["tx"].length,new Date( parseInt(data["data"]["time"])*1000 ));
+    Blocks.push(b);
+  });
+
+
+}
+
 
 
 function getTx(txString){
